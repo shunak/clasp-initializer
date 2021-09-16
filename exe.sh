cd267cfbf811c312b270dc638977f29191634a7b
shunak <shuna966@gmail.com>
2021-09-16 12:08:58 +0900

    wip

--- exe.sh
+++ exe.sh
@@ -1,7 +1,18 @@
 #! /bin/zsh
+
+if [ $# != 1 ]; then
+	echo "Error: Set your Existing project ID (Spreadsheet, Form, and so on...)" $*
+    exit 1
+else
+    echo "Start creating a New clasp project with TypeScript "
+fi
+
 npm init -y
 npm install @google/clasp tslint -D 
 npm install @types/google-apps-script -S
 tslint --init # tslint は必須ではないがグッドマナーとして導入しておこう
+
+clasp login
+clasp clone $1
 
 rm exe.sh
