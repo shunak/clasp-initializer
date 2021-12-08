894e8148b3a7cdf2c6f71bc0dac4ab149bcfaaa9
shunak <shuna966@gmail.com>
2021-12-08 17:39:23 +0900

    Modified: Test execution message: yarn run test -> yarn run jest

--- exe.sh
+++ exe.sh
@@ -18,7 +18,7 @@ fi
 yarn init -y
 yarn add -D @google/clasp tslint 
 yarn add -S @types/google-apps-script
-# Install Test Unit "ts-jest". To test execute, `yarn run test`
+# Install Test Unit "ts-jest". To test execute `yarn run jest`
 # Installing ts-jest https://huafu.github.io/ts-jest/user/install
 yarn add -D jest "@types/jest" ts-jest
 yarn ts-jest config:init 
